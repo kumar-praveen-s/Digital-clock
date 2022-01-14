78f5000935c09bc62378de6d78337e6a35a77d40
Praveenkumar S <srvpraveen002@gmail.com>
2022-01-14 18:39:12 +0530

    Initial commit

--- index.js
+++ index.js
@@ -37,38 +37,38 @@ function setTime(){
         document.getElementById("pic-container").style.backgroundImage="url(./assets/3.png)";
         document.getElementById("text").innerHTML="Good night";
     }
-    function displayvalue(){
-        let v1=document.getElementById("Wake-up-time").options[document.getElementById("Wake-up-time").selectedIndex].text;
-        let v2=document.getElementById("lunch-time").options[document.getElementById("lunch-time").selectedIndex].text;
-        let v3=document.getElementById("nap-time").options[document.getElementById("nap-time").selectedIndex].text;
-        let newElement1=document.createElement("p");
-        let newElement2=document.createElement("p");
-        let newElement3=document.createElement("p");
-        let pd=document.getElementById("option-select-view-container");
-        newElement1.setAttribute("class","style-p1");
-        newElement2.setAttribute("class","style-p1");
-        newElement3.setAttribute("class","style-p1");
-        if(h1=='none'){
-            newElement1.innerHTML="";
-        }
-        else{
-            newElement1.innerHTML="Wake up Time : "+ v1;
-            pd.appendChild(newElement1);
-        }
-        if(h2=='none'){
-            newElement2.innerHTML="";
-        }
-        else{
-            newElement2.innerHTML="Lunch Time : "+ v2;
-            pd.appendChild(newElement2);
-        }
-        if(h3=='none'){
-            newElement3.innerHTML="";
-        }
-        else{
-            newElement3.innerHTML="Nap Time : "+ v3;
-            pd.appendChild(newElement3);
-        }
+    displayvalue(h1,h2,h3);
+}
+function displayvalue(h1,h2,h3){
+    let v1=document.getElementById("Wake-up-time").options[document.getElementById("Wake-up-time").selectedIndex].text;
+    let v2=document.getElementById("lunch-time").options[document.getElementById("lunch-time").selectedIndex].text;
+    let v3=document.getElementById("nap-time").options[document.getElementById("nap-time").selectedIndex].text;
+    let newElement1=document.createElement("p");
+    let newElement2=document.createElement("p");
+    let newElement3=document.createElement("p");
+    let pd=document.getElementById("option-select-view-container");
+    newElement1.setAttribute("class","style-p1");
+    newElement2.setAttribute("class","style-p1");
+    newElement3.setAttribute("class","style-p1");
+    if(h1=='none'){
+        newElement1.innerHTML="";
     }
-    displayvalue();
+    else{
+        newElement1.innerHTML="Wake up Time : "+ v1;
+        pd.appendChild(newElement1);
+    }
+    if(h2=='none'){
+        newElement2.innerHTML="";
+    }
+    else{
+        newElement2.innerHTML="Lunch Time : "+ v2;
+        pd.appendChild(newElement2);
+    }
+    if(h3=='none'){
+        newElement3.innerHTML="";
+    }
+    else{
+        newElement3.innerHTML="Nap Time : "+ v3;
+        pd.appendChild(newElement3);
+    }
 }
